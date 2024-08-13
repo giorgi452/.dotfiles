0a7b00d7971435a841e7b0cd916cfdf77d19e412
giorgi452 <giorgi.milashvili10@gmail.com>
2024-08-13 10:32:58 +0400

    kitty

---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e 
+i3lock --nofork --show-failed-attempts --ignore-empty-password --tiling --image='$HOME/Pictures/wallpapers/wallpaper-9.png ' 
+xset dpms
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -5,6 +5,28 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set smartindent")
+vim.cmd("set termguicolors")
+
+vim.opt.guicursor = ""
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "112"
 
 -- Vim Leader
 vim.g.mapleader = " "
@@ -12,3 +34,4 @@ vim.g.maplocalleader = "\\"
 
 -- Mapping
 vim.keymap.set("n", "<Leader><Leader>", ":nohlsearch<CR>", { desc = "Clear search highlighting" })
+vim.keymap.set("n", "<Leader>s", ":wa<CR>", { desc = "Save all files" })
