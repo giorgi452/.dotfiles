6229e4e7313018f1853459c5a40bdb7b518c64d9
giorgi452 <giorgi.milashvili10@gmail.com>
2024-12-31 06:27:05 +0400

    i3 changes

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e 
-i3lock --nofork --show-failed-attempts --ignore-empty-password --tiling --image='$HOME/Pictures/wallpapers/wallpaper-9.png ' 
+i3lock --nofork --show-failed-attempts --ignore-empty-password --tiling --image='/home/giorgi/Pictures/lock-screen.png' 
 xset dpms
